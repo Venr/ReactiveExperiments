2ee148f06887597b0adeb0688a8fb824e7153bf1
Omar Shaikh <squishymaker@gmail.com>
2012-07-18 09:44:28 -0400

    Observable collection that throttles change notifications using reactive extensions

---
+++ ReactiveCollectionEventThrottle/ReactiveCollectionEventThrottle/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TupleType = System.Tuple<System.Collections.IList, System.Collections.IList>;
+using AccumulatorType = System.Collections.Generic.List<System.Tuple<System.Collections.IList, System.Collections.IList>>;
+using System.Collections;
+using System.Threading;
+using System.ComponentModel;
+
+namespace ReactiveCollectionEventThrottle
+{
+    class Program
+    {
+        private static List<string> newItems = new List<string>();
+        private static List<string> oldItems = new List<string>();
+
+        static ThrottledObservableList<string> collection;
+
+        static void Main(string[] args)
+        {
+            var list = new ObservableCollection<string> { "hello", "world" };
+
+            collection = new ThrottledObservableList<string>(list);
+
+            //var seed = new AccumulatorType();
+
+            var rawEventSource = 
+                Observable.FromEventPattern<NotifyCollectionChangedEventArgs>(collection, "CollectionChanged")
+                .Select(a => a.EventArgs);
+
+            rawEventSource.Subscribe(Publish);
+
+            Observable.FromEventPattern<PropertyChangedEventArgs>(collection, "PropertyChanged")
+                .Select(a => a.EventArgs)
+                .Where(a => a.PropertyName == "Count")
+                .Subscribe(CountChanged);
+
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                foreach (var i in Enumerable.Range(1, 10))
+                {
+                    GenerateEvents(collection);
+                    //GenerateRemoveEvents(collection);
+                    System.Threading.Thread.Sleep(105);
+                }
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void CountChanged(PropertyChangedEventArgs obj)
+        {
+            Console.WriteLine("Current count: {0}", collection.Count);
+        }
+
+        private static void GenerateRemoveEvents(ThrottledObservableList<string> collection)
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                collection.RemoveAt(i);
+            }
+        }
+
+        private static void GenerateEvents(ICollection<string> collection)
+        {
+            var r = new Random(DateTime.Now.GetHashCode());
+            foreach (var i in Enumerable.Range(1, r.Next(100000)))
+            {
+                collection.Add(i.ToString());
+            }
+        }
+
+
+        private static void Publish(NotifyCollectionChangedEventArgs obj)
+        {
+            
+            Console.WriteLine("Collection Modified! action:{0}", obj.Action);
+            if(obj.Action == NotifyCollectionChangedAction.Add)
+                Console.WriteLine("Modified items: {0}", obj.NewItems.Count);
+            else if(obj.Action == NotifyCollectionChangedAction.Remove)
+                Console.WriteLine("Modified items: {0}", obj.OldItems.Count);
+        }
+
+    }
+}
---
+++ ReactiveCollectionEventThrottle/ReactiveCollectionEventThrottle/ThrottledObservableList.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveCollectionEventThrottle
+{
+    public class ThrottledObservableList<T> : IList<T>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        public static readonly TimeSpan DefaultThrottle = TimeSpan.FromMilliseconds(50);
+
+        private ObservableCollection<T> list;
+
+        private List<T> newItems = new List<T>();
+        private List<T> oldItems = new List<T>();
+
+        public ThrottledObservableList() : this(DefaultThrottle) { }
+        public ThrottledObservableList(TimeSpan throttle) : this(throttle, null) { }
+        public ThrottledObservableList(IEnumerable<T> source) : this(DefaultThrottle, source) { }
+        public ThrottledObservableList(TimeSpan throttle, IEnumerable<T> source)
+        {
+            list = new ObservableCollection<T>(source ?? new List<T>());
+
+            var rawEventSource =
+                Observable.FromEventPattern<NotifyCollectionChangedEventArgs>(list, "CollectionChanged")
+                .Select(a => a.EventArgs);
+
+            var throttledEventSource = rawEventSource
+                .Throttle(throttle);
+
+            rawEventSource.Subscribe(Accumulate);
+            throttledEventSource.Subscribe(Publish);
+        }
+
+        private void Publish(NotifyCollectionChangedEventArgs obj)
+        {
+            var newList = System.Threading.Interlocked.Exchange(ref newItems, new List<T>());
+            var oldList = System.Threading.Interlocked.Exchange(ref oldItems, new List<T>());
+
+            var collectionChangeHandler = CollectionChanged;
+            if (collectionChangeHandler != null)
+            {
+
+                if (newList.Any())
+                {
+                    var args = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newList);
+                    collectionChangeHandler(this, args);
+                }
+
+                if (oldList.Any())
+                {
+                    var args = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldList);
+                    collectionChangeHandler(this, args);
+                }
+            }
+
+            var propertyChangedHandler = PropertyChanged;
+            if(propertyChangedHandler != null) 
+            {
+                propertyChangedHandler(this, new PropertyChangedEventArgs("Count"));
+            }
+
+        }
+
+        private void Accumulate(NotifyCollectionChangedEventArgs obj)
+        {
+            if (obj.NewItems != null)
+                newItems.AddRange(obj.NewItems.Cast<T>());
+
+            if (obj.OldItems != null)
+                oldItems.AddRange(obj.OldItems.Cast<T>());
+        }
+
+        public int IndexOf(T item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            list.Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return list.Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
